85ddeb25b6a02a19cd607c7844e2a47063f0a0f6
Isaac Cook <isaac@simpload.com>
2013-03-21 21:00:27 -0500

    Migrated templates

---
+++ org_web/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from org_web.models import Content, News
+
+admin.site.register(Content)
+admin.site.register(News)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='kuacm',
+    version='0.1',
+    author=u'Isaac Cook',
+    author_email='isaac@simpload.com',
+    packages=['org_web', 'acm_site'],
+    install_requires=['django==1.4','south'],
+    license='BSD licence, see LICENCE.txt',
+    description='Simple website for University of Kansas Association for Computing Machinery Chapter',
+    zip_safe=False,
+)
